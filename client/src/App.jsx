b3b25a237808dfead05a949d3d21624567a878f4
Anand Mainali <anand.mainali@cyberarrow.io>
2023-12-02 19:22:14 +0545

    routing and some components are added.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,30 @@
+import {createBrowserRouter,
+  RouterProvider} from 'react-router-dom'
+import Home from './pages/Home'
+import Update from './pages/Update'
+import Register from './pages/Register'
+import Login from './pages/Login'
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home/>
+    },
+    {
+      path:"/update",
+      element:<Update/>
+    },
+    {
+      path:"/login",
+      element:<Login/>
+    },
+    {
+      path:"/register",
+      element:<Register/>
+    }
+  ]);
 function App() {
-  return <div>Hello World!</div>;
+  return <RouterProvider router={router} />
 }
 
 export default App;
